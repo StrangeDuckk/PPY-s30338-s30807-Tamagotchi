af35dfb6ea9430b96d1e35769b43f01a22ab2713
StrangeDuckk <mikusek.kasia@gmail.com>
2025-05-27 00:06:07 +0200

    made tamagotchi class and started main file

--- Tamagotchi.py
+++ Tamagotchi.py
@@ -1,6 +1,22 @@
+from abc import abstractmethod
+
+
 class Tamagotchi:
-    def __init__(self, poziomNajedzenia, poziomNudy):
-        self.najedzenie = poziomNajedzenia//2 #poczatkowo ustawiamy na polowe najedzenia
-        self.maxNajedzenie = poziomNajedzenia
-        self.nuda = poziomNudy//2 #poczatkowo ustawiamy na polowe nudy
-        self.maxNuda = poziomNudy
+    def __init__(self, maxPoziomNajedzenia, maxPoziomNudy, rodzaj):
+        self.najedzenie = maxPoziomNajedzenia//2 #poczatkowo ustawiamy na polowe najedzenia
+        self.maxNajedzenie = maxPoziomNajedzenia
+        self.nuda = maxPoziomNudy//2 #poczatkowo ustawiamy na polowe nudy
+        self.maxNuda = maxPoziomNudy
+        self.rodzajPostaci = rodzaj
+
+    @abstractmethod
+    def specjalnaUmiejetnosc(self) ->None:
+        pass
+
+class ZwierzeDefault(Tamagotchi):
+    def __init__(self):
+        super().__init__(100, 100)
+
+class ZwierzeGrubas(Tamagotchi):
+    def __init__(self):
+        super().__init__(150, 100)
--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-import Tamagotchi
+from Tamagotchi import Tamagotchi
 
 def main():
+    #Init postaci
+    postac = None
+
+    #Stworzenie okna
     root = tk.Tk()
     root.geometry("600x600")
     root.title("PJATK Tamagotchi")
+
+    #Wybor postaci
+    tk.Label(root, text="PJATK Tamagotchi").pack()
+
+    #Przyciski wyboru postaci
+    tk.Button(root, text="Zwyczajniak", command=lambda: start("z")).pack()
+    tk.Button(root, text="Grubas", command=lambda: start("g")).pack()
+    tk.Button(root, text="Dzieciak", command=lambda: start("d")).pack()
+    tk.Button(root, text="Biznesmen", command=lambda: start("b")).pack()
+
+    #
+    def start(wyborPostaci):
+        nonlocal postac
+        if wyborPostaci == "z":
+            postac = Tamagotchi(100,100, "z");
+            print("utworzone zwierzaka z")
+        if wyborPostaci == "g":
+            postac = Tamagotchi(150,100, "g");
+            print("utworzone zwierzaka g")
+        if wyborPostaci == "d":
+            postac = Tamagotchi(100,100, "d");
+            print("utworzone zwierzaka d")
+        if wyborPostaci == "b":
+            postac = Tamagotchi(100,100, "b");
+            print("utworzone zwierzaka b")
+
+
+
 
     root.mainloop()
 
